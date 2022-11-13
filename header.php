3c2b848426e43a16e68d6ae6f23ea3e762380ac5
Rafaelnfs <rafaelnfs261189@gmail.com>
2022-11-12 21:33:28 -0600

    primer impresion de codigo

---
+++ header.php
@@ -0,0 +1,14 @@
+<ul class="nav navbar-right top-nav">
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>  <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+            <li>
+                <a href="perfil.php"><i class="fa fa-fw fa-user"></i> Perfil</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+                <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Cerrar sesion</a>
+            </li>
+        </ul>
+    </li>
+</ul>
---
+++ sidebar.php
@@ -0,0 +1,19 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> DashBoard</a>
+        </li>
+        <li>
+            <a href="cesta.php"><i class="fa fa-ticket"></i> Mis Ticket</a>
+        </li>
+        <li>
+            <a href="cuenta.php"><i class="fa fa-book"></i> Mi Cuenta</a>
+        </li>
+        <li>
+            <a href="alta_subasta.php"><i class="fa fa-archive"></i> Nueva PQRS </a>
+        </li>
+        <li>
+            <a href="alta_categoria.php"><i class="fa fa-bell"></i> PENDIENTE</a>
+        </li>
+    </ul>
+</div>
