35127f333cafcd0069b6071b04beb7d55a084de3
Rafaelnfs <rafaelnfs261189@gmail.com>
2022-11-12 22:29:32 -0600

    login y registro

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+  //Se incluye el archivo Conexion.php que contiene la clase usada para la conexion a la bd
+
+  //include ("conexion/Conexion.php");
+  //Se crea el objeto conexion
+  
+  //$bd = new Conexion();
+  //Se inicia la sesion o se propaga
+  
+  session_start();
+  //Condicion que no deja entrar al index a menos que exista una variable de session
+  
+  if(!isset($_SESSION["id_usuario"])){
+    //Redirecciona al login
+    header("Location: login.php");
+  }
+?>
+
+
 
 <!DOCTYPE html>
 <html lang="es">
@@ -13,7 +32,7 @@
     <title>UPES</title>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.ico">
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
